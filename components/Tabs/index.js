96b6e4d276adc7e087d08025344403e02f317178
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-04-19 19:50:09 -0400

    sent get request using axios, console logged responses and errors using then() and catch()

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,28 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+
+//My Code
+//pseudocode
+    
+
+    //create function to iterate over the topics
+        //create div element
+        //give class 'tab' to div element
+
+        //use .forEach to iterate over each topic
+        //give textContent to div element
+        //append div element to the div.topics element
+        //return each div element
+
+//send GET request
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    //use .then() to console.log the response
+    .then((response) => {
+        console.log(response);
+    })
+    //use .catch() to console.log the error
+    .catch((error) => {
+        console.log(error);
+    });
