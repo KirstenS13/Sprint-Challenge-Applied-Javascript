4917a0b561c4be949bcbe33b14e5b77c030aae02
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-04-19 20:07:41 -0400

    created function to iterate over topics and create tabs for each one, called function in then() under axios GET request, it worked, done with tabs section for now

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -11,24 +11,32 @@
 
 //My Code
 //pseudocode
-    
-
-    //create function to iterate over the topics
-        //create div element
-        //give class 'tab' to div element
-
-        //use .forEach to iterate over each topic
-        //give textContent to div element
-        //append div element to the div.topics element
-        //return each div element
 
 //send GET request
 axios.get('https://lambda-times-backend.herokuapp.com/topics')
     //use .then() to console.log the response
     .then((response) => {
         console.log(response);
+        tabConstructor(response);
     })
     //use .catch() to console.log the error
     .catch((error) => {
         console.log(error);
     });
+
+//create function to iterate over the topics
+function tabConstructor(topicInfoObj) {
+    //use .forEach to iterate over each topic
+    topicInfoObj.data.topics.forEach((topicEntry) => {
+        //create div element
+        const tab = document.createElement('div');
+        //give class 'tab' to div element
+        tab.classList.add('tab');
+        //give textContent to div element
+        tab.textContent = topicEntry;
+
+        //append div element to the div.topics element
+        const topicsContainer = document.querySelector('.topics');
+        topicsContainer.appendChild(tab);
+    })
+};
